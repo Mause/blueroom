d9b42d6f2ec511bb2bfca9ecc6a352447293772b
Elliana May <me@mause.me>
2025-01-26 14:15:55 +0800

    timezones

--- post_process.py
+++ post_process.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 
 from icalendar import Calendar, Event
+from pytz import timezone
 
 with open("out.json") as f:
     shows = json.load(f)
@@ -19,15 +20,22 @@ refresh_interval = "PT1H"
 cal.add("REFRESH-INTERVAL", refresh_interval, parameters={'value': "DURATION"})
 cal.add("X-PUBLISHED-TTL", refresh_interval)
 
+cal.add("X-WR-TIMEZONE", "Australia/Perth")
+
 for show in shows:
     for date in show["dates"]:
         event = Event()
         event.add("uid", show["item_hash"] + ' ' + date[0])
         event.add("summary", show["title"])
-        start = datetime.fromisoformat(date[0])
+        start = datetime.fromisoformat(date[0]).replace(
+            tzinfo=timezone("Australia/Perth")
+        )
         event.add("dtstamp", start)
         event.add("dtstart", start)
-        event.add("dtend", datetime.fromisoformat(date[1]))
+        event.add(
+            "dtend",
+            datetime.fromisoformat(date[1]).replace(tzinfo=timezone("Australia/Perth")),
+        )
         #        event.add('location', show['url'])
         event.add("description", show["url"])  # + '\n\n\n' + show['desc'])
         cal.add_component(event)

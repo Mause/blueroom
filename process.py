59f73ff24d710dfe4bf56c651ebaffbef1c3fa07
Elliana May <me@mause.me>
2025-11-06 18:09:27 +0800

    typo

--- process.py
+++ process.py
@@ -60,7 +60,7 @@ def get_event_url(domain: str, event: FerveItem) -> str:
     if path == "/Events/O-D-E":
         path = "/Events/ODE"
     if domain == "ourgoldenage.com.au":
-        path = path.replace("/Events/", "/films/")
+        path = path.replace("/Events/", "/film/")
     return f"https://{domain}{path.lower()}/"
 
 
